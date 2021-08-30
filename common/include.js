bcd9026f691225ccb22caa741eb7d28ed01ea1f9
tamachika <62124334+tamachika@users.noreply.github.com>
2021-08-30 16:02:08 +0900

    Update include.js

--- common/include.js
+++ common/include.js
@@ -19,7 +19,7 @@ fetch("./common/sidebar.html")
 
 fetch("footer.html")
   .then(response => {
-    return response.text("")
+    return response.text()
   })
   .then(data => {
     document.querySelector("footer").innerHTML = data;
